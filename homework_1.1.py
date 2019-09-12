ad83bbe3ce801d9750ca8703e9b55760f0d7151a
Andrii Piddubchyshyn <andrii.piddubchyshyn@gmail.com>
2019-09-12 13:45:55 +0300

    update 2 files

--- homework_1.1.py
+++ homework_1.1.py
@@ -1,3 +1,11 @@
+"""
+Define variables a and b. Read values a and b from console and calculate:
+a+b
+a - b
+a*b
+a/b
+Output obtained results.
+"""
 a = int(input(' Enter number "a": '))
 b = int(input(' Enter number "b": '))
 print('a + b =',a+b)
--- homework_1.2.py
+++ homework_1.2.py
@@ -1,3 +1,8 @@
+"""
+Output question	"What is your name?", "How old are you?", "Where do you live?".
+Read the answer of user  and output  next information :"Hello, (answer(name))",
+'Your age is (answer(age))', 'You live in (answer(city))'
+"""
 name = input('What is your name? ')
 age = input('How old are you? ')
 city = input('Where do you live? ')
