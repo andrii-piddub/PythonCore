db7243a18757e6479e8879640b9e689f1d9a21cb
Andrii Piddubchyshyn <andrii.piddubchyshyn@gmail.com>
2019-09-16 22:36:00 +0300

    Add class work

--- homework_2.3.py
+++ homework_2.3.py
@@ -4,13 +4,16 @@ b = False
 print('a =',a,'b = ',b)
 a,b = b,a 
 print('a = ',a,'b = ',b)
-
+print("_________________________")
 #Another way to do this
 c  = 345
 d = 'some_text'
+print(id(c),id(d))
 print('c = ',c,'d =',d)
 c = [c]
+print(id(c))
 c.append(d)
 d = c[0]
 c = c[1]
+print(id(c),id(d))
 print('c =',c, 'd = ',d)
